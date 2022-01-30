c4cb5f2adc4b2507d1797e587a909c0b1f2dcc83
akaryaka <a.karyaka.2dit@gmail.com>
2022-01-30 21:06:39 +0600

    desktop without links, images

--- src/pages/Main/components/Content/Content.js
+++ src/pages/Main/components/Content/Content.js
@@ -1,9 +1,14 @@
-
 import React from 'react';
+import './css/Content.css';
 
 function Content() {
   return(
-    <div>Main</div>
+    <div className='Content'>
+      <div className='Content__title'>
+        19º
+      </div>
+      <div className='Content__info'>Преимущественно солнечно</div>
+    </div>
   )
 }
 
--- src/pages/Main/components/Footer/Footer.js
+++ src/pages/Main/components/Footer/Footer.js
@@ -1,9 +1,28 @@
-
 import React from 'react';
+import './css/Footer.css';
 
 function Footer() {
   return(
-    <div>Footer</div>
+    <div className='Footer'>
+      <div className='Footer-inner'>
+        <div className='Footer__box'>
+          <div className='Footer__box_title'>Ветер</div>
+          <div className='Footer__box_description'>5 м/c, западный</div>
+        </div>
+        <div className='Footer__box'>
+          <div className='Footer__box_title'>Давление</div>
+          <div className='Footer__box_description'>752 мм рт. ст.</div>
+        </div>
+        <div className='Footer__box'>
+          <div className='Footer__box_title'>Влажность</div>
+          <div className='Footer__box_description'>60%</div>
+        </div>
+        <div className='Footer__box'>
+          <div className='Footer__box_title'>Вероятность дождя</div>
+          <div className='Footer__box_description'>10%</div>
+        </div>
+      </div>
+    </div>
   )
 }
 
